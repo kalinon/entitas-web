073645ad117c82ca79675879a88ddbc27dfb55b9
Holden Omans <holden.omans@gmail.com>
2022-06-28 12:11:45 -0400

    minor update

--- src/entitas-web/server.cr
+++ src/entitas-web/server.cr
@@ -3,11 +3,11 @@ require "./routes"
 
 module Entitas
   abstract class Controller
-    def start_server
+    def start_server(args = Array(String).new)
       ENTITAS_WEB_SHARED.controller = self
 
       spawn do
-        Kemal.run(Array(String).new)
+        Kemal.run(args)
       end
     end
   end
